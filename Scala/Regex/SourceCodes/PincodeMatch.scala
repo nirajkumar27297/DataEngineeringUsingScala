3d6f757831942d77602b1bf40b5494f55bf07287
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-24 23:27:12 +0530

    Niraj[Refracted]:Scala codes for matching Pincode

--- Scala/Regex/SourceCodes/PincodeMatch.scala
+++ Scala/Regex/SourceCodes/PincodeMatch.scala
@@ -6,21 +6,18 @@ object PincodeMatch {
   def patternMatch(pincode:String): String = {
     val pattern = new Regex("^[1-9][0-9]{2}\\s{0,1}[0-9]{3}$")
     val result =  pattern.findFirstMatchIn(pincode) match {
-      case Some(_) => "Pin Code is Okay"
-      case None => "First three digits should be number followed by an " +
-        "optional space and then three digit number"
+      case Some(_) => "Valid"
+      case None => "Invalid"
     }
     result
   }
   def main(args: Array[String]): Unit = {
     var choice = 'y'
     while(choice.toLower == 'y') {
-      println("Enter 6 digit Pin Code")
-      println("First three digits should be number followed by an " +
+      println("Enter 6 digit Pin Code\nFirst three digits should be number followed by an " +
         "optional space and then three digit number")
       val pincode = scala.io.StdIn.readLine()
-      println(patternMatch(pincode))
-      println("Do you enter one more time,If yes press  y or else anything")
+      println(patternMatch(pincode)+"\nDo you enter one more time,If yes press  y or else anything")
       choice = scala.io.StdIn.readChar()
     }
   }
