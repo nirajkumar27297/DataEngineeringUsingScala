0e619306f5f4c8514946200d21192a0e1c6e812c
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-24 23:18:41 +0530

    Niraj[Refracted]:Scala codes for matching FirstName pattern

--- Scala/Regex/SourceCodes/FirstNameMatch.scala
+++ Scala/Regex/SourceCodes/FirstNameMatch.scala
@@ -5,8 +5,8 @@ object FirstNameMatch {
   def patternMatch(name:String): String = {
     val pattern = new Regex("^[A-Z]{1}[a-z,A-Z]{2,}$")
     val result = pattern.findFirstMatchIn(name) match {
-      case Some(_) => "First Name is Okay"
-      case None => "Invalid !!! First Letter should be capital and length should be atleast three"
+      case Some(_) => "Valid"
+      case None => "Invalid"
 
     }
     result
@@ -14,11 +14,9 @@ object FirstNameMatch {
   def main(args: Array[String]): Unit = {
     var choice = 'y'
     while(choice.toLower == 'y') {
-      println("Enter First Name")
-      println("First Letter should be capital and length should be atleast three")
+      println("Enter First Name\nFirst Letter should be capital and length should be atleast three")
       val name = scala.io.StdIn.readLine()
-      print(patternMatch(name))
-      println("Do you enter one more time,If yes press  y or else anything")
+      println(patternMatch(name)+"\nDo you enter one more time,If yes press  y or else anything")
       choice = scala.io.StdIn.readChar()
       }
     }
