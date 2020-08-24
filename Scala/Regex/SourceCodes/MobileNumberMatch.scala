682f6fcbdd3e8f6ab3847dbcfe0e15442e3ccb97
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-24 23:21:58 +0530

    Niraj[Refracted]:Scala codes for matching MobileNumber pattern

--- Scala/Regex/SourceCodes/MobileNumberMatch.scala
+++ Scala/Regex/SourceCodes/MobileNumberMatch.scala
@@ -6,20 +6,17 @@ object MobileNumberMatch {
   def patternMatch(mobileNumber:String): String = {
     val pattern = new Regex("^[1-9]{2}\\s[1-9]{1}[0-9]{9}$")
     val result = pattern.findFirstMatchIn(mobileNumber) match {
-      case Some(_) => "Mobile Number is Okay"
-      case None => "Invalid !!! First two digits should be country code followed by a space " +
-        "and latter 10 digits mobile number"
+      case Some(_) => "Valid"
+      case None => "Invalid"
     }
     result
   }
   def main(args: Array[String]): Unit = {
     var choice = 'y'
     while(choice.toLower == 'y') {
-      println("Enter Mobile Number")
-      println("First two digits should be country code followed by a space and latter 10 digits mobile number")
+      println("Enter Mobile Number\nFirst two digits should be country code followed by a space and latter 10 digits mobile number")
       val mobileNumber = scala.io.StdIn.readLine()
-      println(patternMatch(mobileNumber))
-      println("Do you enter one more time,If yes press  y or else anything")
+      println(patternMatch(mobileNumber)+"\nDo you enter one more time,If yes press  y or else anything")
       choice = scala.io.StdIn.readChar()
     }
   }
