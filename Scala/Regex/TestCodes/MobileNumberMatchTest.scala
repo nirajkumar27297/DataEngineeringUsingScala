0c667fde67bab0cd4d5da017559c7a06f381c4b5
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-24 23:34:22 +0530

    Niraj[Refracted]:Scala test codes for matching MobileNumber

--- Scala/Regex/TestCodes/MobileNumberMatchTest.scala
+++ Scala/Regex/TestCodes/MobileNumberMatchTest.scala
@@ -3,17 +3,14 @@ import org.scalatest.FunSuite
 
 class MobileNumberMatchTest extends FunSuite
 {
-  test("MobileNumberMatchingCorrect") {
-    assert(MobileNumberMatch.patternMatch("91 8249123293") == "Mobile Number is Okay")
+  test("GivenStringInput_WhenMobileNumberContainsCountryCodeFollowedBySpaceAndTenDigitsNumber_ReturnValid") {
+    assert(MobileNumberMatch.patternMatch("91 8249123293") == "Valid")
   }
-  test("MobileNumberInCorrectPattern"){
-    assert(MobileNumberMatch.patternMatch("918249123293") == "Invalid !!! First two digits should be country code followed by a space " +
-      "and latter 10 digits mobile number")
+  test("GivenStringInput_WhenMobileNumberContainsCountryCodeFollowedByNonSpaceAndTenDigitsNumber_ReturnInvalid"){
+    assert(MobileNumberMatch.patternMatch("918249123293") == "Invalid")
   }
-  test(testName = "MobileNumberIncorrectSpace"){
-    assert(MobileNumberMatch.patternMatch("91 8249123") ==
-      "Invalid !!! First two digits should be country code followed by a space " +
-        "and latter 10 digits mobile number")
+  test(testName = "GivenStringInput_WhenMobileNumberContainsCountryCodeFollowedBySpaceAndButNotTenDigitsNumber_ReturnInvalid"){
+    assert(MobileNumberMatch.patternMatch("91 8249123") == "Invalid")
   }
 
 }
