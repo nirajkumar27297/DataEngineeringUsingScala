4914b8803aab4dc3b63bd2fa72fe4cb1e7df7d6f
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-08-24 23:17:27 +0530

    Niraj[Refracted]:Scala codes for emailid pattern match

--- Scala/Regex/SourceCodes/EmailIDMatch.scala
+++ Scala/Regex/SourceCodes/EmailIDMatch.scala
@@ -6,9 +6,8 @@ object EmailIDMatch {
   def patternMatch(emailID:String): String = {
     val pattern = new Regex("^[0-9a-zA-z]+([.+_-][0-9a-zA-z]+)*@[0-9a-zA-z]+.[a-zA-Z]{2,4}([.][a-zA-z]{2,4})*$")
     val result = pattern.findFirstMatchIn(emailID) match {
-      case Some(_) => "Email Id is Okay"
-      case None => "Invalid !!! Email ID should be in form abc@example.com"
-
+      case Some(_) => "Valid"
+      case None => "Invalid"
     }
     result
   }
@@ -17,8 +16,7 @@ object EmailIDMatch {
     while(choice.toLower == 'y') {
       println("Enter Email ID")
       val emailID = scala.io.StdIn.readLine()
-      println(patternMatch(emailID))
-      println("Do you enter one more time,If yes press  y or else anything")
+      println(patternMatch(emailID)+"\nDo you enter one more time,If yes press  y or else anything")
       choice = scala.io.StdIn.readChar()
     }
   }
