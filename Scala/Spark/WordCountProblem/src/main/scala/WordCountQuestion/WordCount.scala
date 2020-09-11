599afa9af404d3b31ec6617d69c162ffc99e5160
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-09-11 23:59:46 +0530

    [Niraj]Refracted:Scala source codes  to include wordCount through dataframe

--- Scala/Spark/WordCountProblem/src/main/scala/WordCountQuestion/WordCount.scala
+++ Scala/Spark/WordCountProblem/src/main/scala/WordCountQuestion/WordCount.scala
@@ -1,40 +1,54 @@
 package WordCountQuestion
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{explode, split}
 import org.apache.spark.{SparkConf, SparkContext}
 
-object WordCount {
+class WordCount {
 
-  val conf = new SparkConf().setAppName("Word Count").setMaster("local[*]")
-  val hadoopConf = new Configuration()
-  val fs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
-
-  private def dirExists(hdfsDirectory:String):Boolean = {
-    fs.exists(new Path(hdfsDirectory)) && fs.isDirectory(new Path(hdfsDirectory))
+  private def generateTime(): String = {
+    val today = Calendar.getInstance().getTime()
+    val form = new SimpleDateFormat("dd / MM / yy")
+    val hr24 = new SimpleDateFormat("HH")
+    val formhr24 = hr24.format(today.getTime)
+    val min = new SimpleDateFormat("mm")
+    val formmin = min.format(today.getTime)
+    val sec = new SimpleDateFormat("ss")
+    val formsec = sec.format(today.getTime)
+    val time  = (form.format(today)+","+formhr24+" : "+formmin+" : "+formsec).toString
+    time
   }
 
-  def countWords(sc: SparkContext,inputFile:String):(RDD[(String, Int)], Map[String, Int]) =  {
-    val rawData = sc.textFile(inputFile)
+  def countWordsRDD(sparkContext: SparkContext,inputFile:String,outputPath:String):Map[String, Int] =  {
+
+    val rawData = sparkContext.textFile(inputFile)
     val words = rawData.flatMap(line => line.split(" "))
     val wordsKv = words.map(word => (word, 1))
-    val output = wordsKv.reduceByKey(_ + _)
-    println(output.collect().toMap)
-    (output,output.collect().toMap)
+    val outputRDD = wordsKv.reduceByKey(_ + _)
+    println(outputRDD.collect().toMap)
+    val currentTime = generateTime()
+    println(outputPath+currentTime.trim)
+    outputRDD.saveAsTextFile(outputPath+currentTime)
+    println(outputRDD.collect().toMap)
+    outputRDD.collect().toMap
   }
 
-  def main(args: Array[String]): Unit = {
-    val sc = new SparkContext(conf)
-    val outputRDDTuple = countWords(sc,args(0))
-    if(dirExists(args(1)) == true) {
-      println("Directory Exists!!!")
-      fs.delete(new Path(args(1)),true)
-    }
-    outputRDDTuple._1.saveAsTextFile(args(1))
-    sc.stop()
+  def countWordsDataFrame(spark:SparkSession,inputFile:String):DataFrame = {
+    val textDf = spark.read.text(inputFile)
+    val wordsDf = textDf.select(explode(split(textDf("value")," ")).alias("word"))
+    val countDf = wordsDf.groupBy("word").count()
+    countDf.show(10)
+    countDf
   }
 }
+
+
 
 
 
