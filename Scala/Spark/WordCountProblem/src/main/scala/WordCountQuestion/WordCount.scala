fd51feb7250149baaf687cb1e43a5f316e126256
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-09-12 10:41:27 +0530

    [Niraj]Refractor:Scala source codes to add dateTime class

--- Scala/Spark/WordCountProblem/src/main/scala/WordCountQuestion/WordCount.scala
+++ Scala/Spark/WordCountProblem/src/main/scala/WordCountQuestion/WordCount.scala
@@ -1,29 +1,18 @@
 package WordCountQuestion
 
-import java.text.SimpleDateFormat
-import java.util.Calendar
-
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
-import org.apache.spark.rdd.RDD
+import DateTimeGenerator.GenerateDateTime
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.{explode, split}
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext
 
 class WordCount {
 
-  private def generateTime(): String = {
-    val today = Calendar.getInstance().getTime()
-    val form = new SimpleDateFormat("dd / MM / yy")
-    val hr24 = new SimpleDateFormat("HH")
-    val formhr24 = hr24.format(today.getTime)
-    val min = new SimpleDateFormat("mm")
-    val formmin = min.format(today.getTime)
-    val sec = new SimpleDateFormat("ss")
-    val formsec = sec.format(today.getTime)
-    val time  = (form.format(today)+","+formhr24+" : "+formmin+" : "+formsec).toString
-    time
+  private def getDateTime():String = {
+    val generatedateTimeObj = new GenerateDateTime()
+    val currentTime = generatedateTimeObj.generateTimeDDMMYYFormat()
+    currentTime
   }
+
 
   def countWordsRDD(sparkContext: SparkContext,inputFile:String,outputPath:String):Map[String, Int] =  {
 
@@ -32,18 +21,18 @@ class WordCount {
     val wordsKv = words.map(word => (word, 1))
     val outputRDD = wordsKv.reduceByKey(_ + _)
     println(outputRDD.collect().toMap)
-    val currentTime = generateTime()
-    println(outputPath+currentTime.trim)
+    val currentTime = getDateTime()
     outputRDD.saveAsTextFile(outputPath+currentTime)
-    println(outputRDD.collect().toMap)
     outputRDD.collect().toMap
   }
 
-  def countWordsDataFrame(spark:SparkSession,inputFile:String):DataFrame = {
+  def countWordsDataFrame(spark:SparkSession,inputFile:String,outputPath:String):DataFrame = {
     val textDf = spark.read.text(inputFile)
     val wordsDf = textDf.select(explode(split(textDf("value")," ")).alias("word"))
     val countDf = wordsDf.groupBy("word").count()
     countDf.show(10)
+    val currentDateTime = getDateTime()
+    countDf.write.csv(outputPath+currentDateTime)
     countDf
   }
 }
